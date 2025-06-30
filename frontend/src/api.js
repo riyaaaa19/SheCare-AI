dfc7a933dd256976aa9d9b598b4a535135fd4f7a
Shreyasaraf01 <shreyasaraf765@gmail.com>
2025-06-30 21:24:18 +0530

    final

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -72,4 +72,4 @@ export const getRecommendations = async () => {
 export const get = (url, config) => api.get(url, config);
 export const post = (url, data, config) => api.post(url, data, config);
 export const put = (url, data, config) => api.put(url, data, config);
-export const del = (url, config) => api.Delete(url, config);
+export const del = (url, config) => api.delete(url, config);
